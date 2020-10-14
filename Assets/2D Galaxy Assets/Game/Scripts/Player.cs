dda0b5b1d2e36d1de1b686b9f0502425f226c6b5
Breno Abreu <brenogpa@gmail.com>
2020-10-14 14:13:15 -0300

    Player's starting position

---
+++ Assets/2D Galaxy Assets/Game/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        //current position = new position 
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+
+    }
+}
